fdd95e7374ace0ae5f2fdda2904570042f7b7fa6
Matei Stefanica <matei.stefanica@softwire.com>
2026-07-16 17:09:17 +0300

    added angry rabbit telling you to go home

--- src/components/basic-counter.tsx
+++ src/components/basic-counter.tsx
@@ -1,10 +1,25 @@
 import { useState, useEffect } from 'react';
 import { setItem, getItem } from '../utils.tsx';
+import RabbitContent from './rabbit-content'
+import rabbitImage from '../assets/rabbit.png'
 
 function Counter() {
     const [count, setCount] = useState(() => {
         return getItem('count') || 0;
     });
+    const images : string[] = [
+        String(rabbitImage),
+        String(rabbitImage),
+        String(rabbitImage),
+        String(rabbitImage),
+        String(rabbitImage),
+    ]
+    
+    const handleClick = () => {
+        if (count < 5) {
+            setCount(count + 1);
+        }
+    };
 
     useEffect(() => {
         setItem('count', count);
@@ -13,9 +28,17 @@ function Counter() {
     return (
         <div>
             <h1>{count}</h1>
-            <button onClick={() => setCount(count + 1)}>Increment</button>
+            <button onClick={() => handleClick()}>Increment</button>
+            <div>
+                {images.slice(0, count).map((imgage, index) =>
+                    <img key={index} src={imgage} width="100em"/>
+                )}
+                <h3>
+                    Stop looking at NASA and go home
+                </h3>
+            </div>
         </div>
-        );
-    }
+    );
+}
 
 export default Counter;
---
+++ src/components/rabbit-content.tsx
@@ -0,0 +1,20 @@
+import React, {useState} from 'react';
+import styled from 'styled-components';
+import BasicContentProps from './content-interfaces'
+import rabbitImage from '../assets/rabbit.png'
+
+
+const Image = styled.img`
+  width: 10em;
+  height: auto;
+  padding-top: 1.5em;
+  text-align: center;
+`;
+
+const RabbitContent = () => {
+    return (
+        <Image src={rabbitImage}/>
+      )
+}
+
+export default RabbitContent
