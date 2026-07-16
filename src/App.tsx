0d82a1582de9207d34c9363065fd926ebd6c7688
Matei Stefanica <matei.stefanica@softwire.com>
2026-07-16 15:41:29 +0300

    removed unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import './App.css'
 import BasicContent from './components/basic-content'
 import Counter from './components/basic-button'
 import {FIRST_NASA_PARAGRAPH, SECOND_NASA_PARAGRAPH, NASA_IMAGE} from './assets/nasa-content'
@@ -11,7 +10,6 @@ function App() {
                   firstParagraphContent={FIRST_NASA_PARAGRAPH}
                   secondParagraphContent={SECOND_NASA_PARAGRAPH}
                   imagePath={NASA_IMAGE}/>
-
     </>
   )
 }
